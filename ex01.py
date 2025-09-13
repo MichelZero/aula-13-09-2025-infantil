cbd7453ff86ef2086dcc2d9694d9b2b0fdd1dcc7
MichelZero <silva.michel@ifpb.edu.br>
2025-09-13 18:01:40 -0300

    up

---
+++ ex01.py
@@ -0,0 +1,19 @@
+#
+# autor: Michel 
+# data: 13/09/2025
+
+valor1 = False
+valor2 = False
+valor3 = False
+valor4 = False
+
+if valor1:
+  print("ComandoA")
+elif valor2:
+  print("ComandoB")
+elif valor3:
+  print("ComandoC")
+elif valor4:
+  print("ComandoD")
+else:
+  print("Comando-fim")
